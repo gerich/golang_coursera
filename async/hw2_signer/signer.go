9464e9a7ece045d1899b2dfe1694a07074a27c9e
Alexandr Generalov <a.generalov@muztorg.ru>
2018-06-28 23:10:55 +0300

    sync

--- async/hw2_signer/signer.go
+++ async/hw2_signer/signer.go
@@ -1,3 +1,28 @@
 package main
 
-// сюда писать код
+import (
+	"sort"
+)
+
+// SingleHash single hash
+func SingleHash(in, out chan) job {
+	return DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
+}
+
+// MultiHash multi hash
+func MultiHash() job {
+	return data
+}
+
+func CombineResults(data []string) job {
+	sort.Strings(data)
+	for _, str := range data {
+		res += str + "_"
+	}
+	return res[:len(res)-1]
+}
+
+//ExecutePipeline execute pipeline
+func ExecutePipeline(jobs ...job) {
+
+}
