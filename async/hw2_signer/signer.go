7c5a0a7316e54808b724fc2051297fd9446dfff5
Alexandr Generalov <a.generalov@muztorg.ru>
2018-07-06 00:07:39 +0300

    sync

--- async/hw2_signer/signer.go
+++ async/hw2_signer/signer.go
@@ -1,28 +1,44 @@
 package main
 
+import (
+	"sort"
+	"strconv"
+	"sync"
+)
+
 // SingleHash single hash
 func SingleHash(in, out chan interface{}) {
-	data := <-in
-	str, _ := data.(string)
-	out <- DataSignerCrc32(str) + "~" + DataSignerCrc32(DataSignerMd5(str))
+	for data := range in {
+		intData, _ := data.(int)
+		str := strconv.Itoa(intData)
+		out <- (DataSignerCrc32(str) + "~" + DataSignerCrc32(DataSignerMd5(str)))
+	}
 }
 
 // MultiHash multi hash
 func MultiHash(in, out chan interface{}) {
-	res := ""
-	data := <-in
-	str, _ := data.(string)
-	for index := 0; index < 6; index++ {
-		res += DataSignerCrc32(string(index) + str)
+	for data := range in {
+		res := ""
+		str, _ := data.(string)
+		for index := 0; index < 6; index++ {
+			res += DataSignerCrc32(string(index) + str)
+		}
+		out <- res
 	}
-	out <- res
 }
 
 // CombineResults combine results
 func CombineResults(in, out chan interface{}) {
-	res := ""
+	store := []string{}
+
 	for data := range in {
 		str, _ := data.(string)
+		store = append(store, str)
+	}
+
+	sort.Strings(store)
+	res := ""
+	for _, str := range store {
 		res += str + "_"
 	}
 	out <- res[:len(res)-1]
@@ -30,12 +46,18 @@ func CombineResults(in, out chan interface{}) {
 
 //ExecutePipeline execute pipeline
 func ExecutePipeline(jobs ...job) {
-	out := make(chan interface{}, 100)
-	in := make(chan interface{}, 100)
+	out := make(chan interface{})
+	in := make(chan interface{})
+	wg := &sync.WaitGroup{}
 	for _, curr := range jobs {
-		curr(in, out)
+		wg.Add(1)
+		go func(in, out chan interface{}, curr job, wg *sync.WaitGroup) {
+			defer wg.Done()
+			defer close(out)
+			curr(in, out)
+		}(in, out, curr, wg)
 		in = out
-		close(out)
-		out = make(chan interface{}, 100)
+		out = make(chan interface{})
 	}
+	wg.Wait()
 }
