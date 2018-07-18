a81ecc793f230d7882f6b156f7fa2211ef705e72
Alexandr Generalov <a.generalov@muztorg.ru>
2018-07-18 23:40:30 +0300

    sync

--- 4/hw4_test_coverage/client_test.go
+++ 4/hw4_test_coverage/client_test.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 )
 
 func HandleSearch(w http.ResponseWriter, r *http.Request) {
+	var users []User
+	file, _ := ioutil.ReadFile("dataset.xml")
 
+	xml.Unmarshal(file, &users)
+
+	fmt.Println(users)
 }
 
 func SearchServer() {
 	ts := httptest.NewServer(http.HandlerFunc(HandleSearch))
+
 }
